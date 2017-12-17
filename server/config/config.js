41b07d19c76042001febc2007a2b0bacb7ca3585
Icemic <bingfeng.jw@gmail.com>
2017-12-17 21:06:25 +0800

    bind to host ip

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   mongo: {
-    host: '127.0.0.1',
+    host: '172.17.0.1',
     port: 27017,
     name: 'search',
   },
   redis: {
     sessiondb: 0,
     memorizedb: 1,
-    host: '127.0.0.1',
+    host: '172.17.0.1',
     port: 6379
   },
   port: 3000
